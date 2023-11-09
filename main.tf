4463d8978993ed56b2b0e34d14d47f1203e13853
Kaviya901053 <150117827+Kaviya901053@users.noreply.github.com>
2023-11-09 12:04:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "linux" {
   most_recent = true
-  owners = ["691879165105"]
+  owners = ["amazon"]
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "linux" {
+  ami           = data.aws_ami.linux.id
   instance_type = var.instance_type
 
   tags = {
