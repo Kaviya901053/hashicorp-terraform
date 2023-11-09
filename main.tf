b052246826cc8ba8832e39d68000ce193b42000b
Kaviya901053 <150117827+Kaviya901053@users.noreply.github.com>
2023-11-09 11:59:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,18 +7,6 @@ provider "aws" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["al2023-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["691879165105"] # Canonical
 }
 
 resource "aws_instance" "ubuntu" {
