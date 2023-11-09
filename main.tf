9259f2c3111f40797d5501af2c78568e0e008d42
Kaviya901053 <150117827+Kaviya901053@users.noreply.github.com>
2023-11-09 11:40:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["al2023-ami-2023*"]
   }
 
   filter {
